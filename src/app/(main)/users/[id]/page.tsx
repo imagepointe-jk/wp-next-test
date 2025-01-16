074361dfebdd065a06220ce866bae4a8f47031e2
imagepointe-jk <148394472+imagepointe-jk@users.noreply.github.com>
2025-01-16 10:49:52 -0600

    Refactor and clean up

--- src/app/(main)/users/[id]/page.tsx
+++ src/app/(main)/users/[id]/page.tsx
@@ -1,8 +1,5 @@
-import {
-  validateUserJwt,
-  validateUserResponse,
-} from "@/types/validation/wpgraphql";
-import { jwtDecode } from "jwt-decode";
+import { checkTokenAgainstUser } from "@/utility/auth";
+
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
@@ -12,39 +9,18 @@ type Props = {
 export default async function Page({ params }: Props) {
   const { id: idFromUrl } = await params; //in next 15 params is now async
   const cookieStore = await cookies();
+
   const token = `${cookieStore.get("wp_jwt_auth")?.value}`;
-  const tokenDecoded = jwtDecode(token);
-  const jwtParsed = validateUserJwt(tokenDecoded);
-  const idFromJwt = jwtParsed.data.user.id;
-  const user = await fetch(process.env.WP_GRAPHQL_URL!, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify({
-      query: `
-            query getUser {
-              user(id:"${idFromUrl}") {
-                id
-                databaseId
-                email
-                name
-              }
-            }
-          `,
-    }),
-  });
-  const json = await user.json();
-  const parsed = validateUserResponse(json);
+  const { match: tokenBelongsToRequestedUser, user } =
+    await checkTokenAgainstUser(token, idFromUrl);
 
   //deny access if the logged in user is trying to access someone else's page. Note that even if it is accessed, WPGraphQL will hide the other person's sensitive info like email.
-  if (parsed.data?.user?.databaseId !== +idFromJwt) redirect("/access-denied");
+  if (!tokenBelongsToRequestedUser) redirect("/access-denied");
 
   return (
     <>
-      <h1>Hello {parsed.data?.user?.name || "UNKNOWN"}</h1>
-      <div>Email: {parsed.data?.user?.email || "UNKNOWN"}</div>
+      <h1>Hello {user.data?.user?.name || "UNKNOWN"}</h1>
+      <div>Email: {user.data?.user?.email || "UNKNOWN"}</div>
     </>
   );
 }
--- src/utility/auth.ts
+++ src/utility/auth.ts
@@ -1,3 +1,9 @@
+import { getUser } from "@/fetch/wordpress";
+import {
+  validateUserJwt,
+  validateUserResponse,
+} from "@/types/validation/wpgraphql";
+import { jwtDecode } from "jwt-decode";
 import { NextResponse } from "next/server";
 
 export function forceLogoutWithResponse(response: NextResponse) {
@@ -7,4 +13,27 @@ export function forceLogoutWithResponse(response: NextResponse) {
   response.cookies.set("wp_user_id", "n/a", {
     expires: 0,
   });
+}
+
+//uses the given token to make an authenticated request for the given user (by the WordPress global id string).
+//if the user data received contains the same (numberic) databaseId as the token, then this token belongs to the given user.
+export async function checkTokenAgainstUser(
+  token: string,
+  userGlobalId: string
+) {
+  const tokenDecoded = jwtDecode(token);
+  const tokenParsed = validateUserJwt(tokenDecoded);
+  const idFromToken = +tokenParsed.data.user.id;
+
+  const userResponse = await getUser(token, userGlobalId);
+  const userJson = await userResponse.json();
+  const userParsed = validateUserResponse(userJson);
+  const userIdFromDatabase = userParsed.data?.user?.databaseId;
+
+  return {
+    idFromToken,
+    user: userParsed,
+    match:
+      userIdFromDatabase !== undefined && userIdFromDatabase === idFromToken,
+  };
 }
